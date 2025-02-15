ee6bd716e319fe4b6e73e3e991b7ce72d5a3fb18
Tomás Antunes <tomasantunes@gmail.com>
2025-02-15 10:44:45 +0000

    Implement Programmable Interrupt Controller

--- src/kernel.asm
+++ src/kernel.asm
@@ -16,9 +16,23 @@ _start:
     mov ebp, 0x00200000
     mov esp, ebp
 
+    ; Enbale the A20 line
     in al, 0x92
     or al, 2
     out 0x92, al
+
+    ; Remap the master PIC
+    mov al, 00010001b
+    out 0x20, al ; Tell master PIC
+
+    mov al, 0x20 ; Interrupt 0x20 is where master ISR should start
+    out 0x21, al
+
+    mov al, 00000001b
+    out 0x21, al
+    ; End remap of the master PIC
+
+    sti
 
     call kernel_main
     jmp $
