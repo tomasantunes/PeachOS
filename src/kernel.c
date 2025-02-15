5bb7836e3a2e62f2183424a39ef496af00193d96
Tomás Antunes <tomasantunes@gmail.com>
2025-02-15 18:15:12 +0000

    Implement Heap

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include "idt/idt.h"
 #include "io/io.h"
+#include "memory/heap/kheap.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -60,6 +61,9 @@ void print(const char* str) {
 void kernel_main() {
     terminal_initialize();
     print("Hello world!\ntest");
+
+    // Initialize the heap
+    kheap_init();
 
     // Initialize the interrupt descriptor table
     idt_init();
